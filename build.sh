31a89a8bca1ffb9fb9bb2523c782fa2747aa0714
Xiaoxu Guo <ftiasch0@gmail.com>
2020-05-14 18:35:50 +0800

    added build commands

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-mkdir build
-pushd build
-  cmake -DMULTICORE=ON -DUSE_PT_COMPRESSION=OFF $EXTRA_CMAKE_ARGS_FOR_CI ..
-  make -j12 main generate_parameters cuda_prover_piecewise
-popd
-mv build/libsnark/main .
-mv build/libsnark/generate_parameters .
-mv build/cuda_prover_piecewise .
+if [[ ! -d build ]]; then
+  mkdir -p build
+  cd build
+  cmake -DMULTICORE=ON -DUSE_PT_COMPRESSION=OFF ..
+  cd ..
+fi
+cd build
+if ! make -j main generate_parameters cuda_prover_piecewise; then
+  exit 1
+fi
+cd ..
+if [[ ! -d build/Debug ]]; then
+  cmake -B build/Debug -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=YES -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_CUDA_COMPILER=clang -G Ninja
+fi
